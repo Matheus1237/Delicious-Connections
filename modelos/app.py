ad12d91e498762646b027ef518cee68096a26070
Matheus1237 <masian202207@icloud.com>
2024-06-24 11:31:48 -0300

    k

---
+++ modelos/app.py
@@ -0,0 +1,21 @@
+from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+bebida_suco = Bebida('Suco de Melancia', 5, 'grande')
+prato_paozinho = Prato('Paozinho', 2, 'O melhor pão da cidade')
+bebida_suco.aplicar_desconto()
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapip(bebida_suco)
+restaurante_praca.adicionar_no_cardapip(prato_paozinho)
+
+
+
+def main():
+    restaurante_praca.exibir_cardapio
+
+
+if __name__ == '__main__':
+    main()
